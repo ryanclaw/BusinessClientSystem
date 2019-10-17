ce7b28577eaba11bf3061c8ee62ec74bb226c941
Ryan Fernandez <rfernandez@rapidrtc.com>
2019-10-17 13:55:36 -0500

    change db schema

--- Models/Clients.cs
+++ Models/Clients.cs
@@ -10,11 +10,13 @@ namespace BusinessClientSystem.Models
         public string firstname {get; set;}
         public string lastname {get; set;}
         public string gender {get; set;}
+        public string contacttype {get; set;}
         public string dateofbirth {get; set;}
         public string address1 {get; set;}
         public string address2 {get; set;}
         public int phone1 {get; set;}
         public int phone2 {get; set;}
         public string email {get; set;}
+        public string product {get; set;}
     }
 }
